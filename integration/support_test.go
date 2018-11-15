76401f4d9d3f6f463b604f72ad1e9cc3c84fa574
Mark Phelps <mark.aaron.phelps@gmail.com>
2018-11-15 11:07:53 -0500

    Fix integration tests

--- integration/support_test.go
+++ integration/support_test.go
@@ -15,11 +15,11 @@ const (
 	organizationName = "codeship"
 	organizationUUID = "1c150f00-e93d-0133-b53e-76bef8d7b14f"
 
-	proProjectName = "codeship/codeship-go"
-	proProjectUUID = "c38f3280-792b-0135-21bb-4e0cf8ff365b"
+	proProjectName = "codeship/codeship-tool-examples"
+	proProjectUUID = "5eda0420-40c6-0133-ef9c-0e8a33e740fc"
 
-	basicProjectName = "codeship/shipscope"
-	basicProjectUUID = "688357c0-6652-0135-f3b7-1268853457c2"
+	basicProjectName = "codeship/merrygoround"
+	basicProjectUUID = "20a13930-c925-0134-952e-3a0fd8dae151"
 )
 
 var (
